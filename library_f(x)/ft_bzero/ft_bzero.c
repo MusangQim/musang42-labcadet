c0349b3eaf9e1c7f2fabef28c08697e809f3a9f4
MusangQim <musangqim03@gmail.com>
2025-11-06 00:37:16 +0800

    update files

--- library_f(x)/ft_bzero/ft_bzero.c
+++ library_f(x)/ft_bzero/ft_bzero.c
@@ -6,8 +6,20 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 22:59:01 by adzmusta          #+#    #+#             */
-/*   Updated: 2025/11/05 22:59:05 by adzmusta         ###   ########.fr       */
+/*   Updated: 2025/11/06 00:35:47 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_bzero(void *erase, size_t bytes)
+{
+	unsigned char	*ptr;
+	int				i;
 
+	ptr = (unsigned char *)erase;
+	i = 0;
+	while (i < bytes)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+}
